c5ad80a1fb9ed5b77a14e878492abe5ee97a6c2a
Graciele Sampaio <graciele-1996@hotmail.com>
2022-10-10 19:28:44 -0300

    requisito 4

--- src/middlewares/validateLoginMiddleware.js
+++ src/middlewares/validateLoginMiddleware.js
@@ -3,17 +3,18 @@ const validateLogin = async (req, res, next) => {
   const emailValid = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
   
   if (!email) {
-    return res.status(400).json({ message: 'O email deve ser preenchido' });
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
-
   if (!emailValid.test(email)) {
-    return res.status(400).json({ message: 'O email deve ter o formato "email@email.com"' });
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"',
+  });
   }
   if (!password) {
-    return res.status(400).json({ message: 'A senha deve ser preenchida' });
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
   }
   if (password.length < 6) {
-    return res.status(400).json({ message: 'A senha deve ter no mínimo 6 caracteres' });
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres',
+   });
   }
   next();
 };
